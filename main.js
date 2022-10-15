4bc784276eac2068da518392284e11a85705fd18
Lathadarsh <97331941+Lathadarsh@users.noreply.github.com>
2022-10-15 17:38:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload() {
+    clown_nose = loadImage("https://i.postimg.cc/YShJsj59/clown-nose-on-white-background-sticker-1601854535-4041057-removebg-preview.png");
 }
 
 function setup() {
@@ -16,13 +20,16 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX= results[0].pose.nose.x;
+        noseY= results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
 
 function draw() {
     image(video,0,0,300,300);
+    image(clown_nose, noseX-15, noseY-10, 25,25);
 }
 
 function take_snapshot() {
